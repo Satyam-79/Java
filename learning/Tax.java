6f07549a2e0c8753ef1e57d631092fc8aa981189
Satyam-79 <satyamverma79@gmail.com>
2021-09-27 15:03:22 +0530

    '

---
+++ learning/Tax.java
@@ -0,0 +1,66 @@
+class Tax
+{
+  public static void main(String args[])
+  {
+    int Income = Integer.parseInt(args[0]);
+    string s=args[1];
+char c=args.charAt(1);
+    char Category = ;
+    int Tax;
+    if(Category = )
+    {
+        if(Income<=180000)
+        {
+            Tax = 0;
+            System.out.println("Tax is : "+Tax);
+        }
+        else if(Income<=500000)
+        {
+             Tax = 180000*0 + (Income - 180000) *0.10;
+             System.out.println("Tax is : "+Tax);
+        }
+        else if(Income<=800000)
+        {
+             Tax = 180000*0+500000*0.10+ (Income - 500000) *0.20;
+             System.out.println("Tax is : "+Tax);
+        }
+        else
+        {
+             Tax = 180000*0+500000*0.10+800000*0.20 (Income - 800000) *0.30;
+             System.out.println("Tax is : "+Tax);
+        }
+    }
+    else if(Category = Women)
+    {
+        if(Income<=190000)
+        {
+            Tax = 0;
+            System.out.println("Tax is : "+Tax);
+        }
+        else if(Income<=500000)
+        {
+             Tax = 180000*0+ (Income - 180000) *0.10;
+             System.out.println("Tax is : "+Tax);
+        }
+        else if(Income<=800000)
+        {
+             Tax = 180000*0+500000*0.10+ (Income - 500000) *0.20;
+             System.out.println("Tax is : "+Tax);
+        }
+        else
+        {
+             Tax = 180000*0+500000*0.10+800000*0.20 (Income - 800000) *0.30;
+             System.out.println("Tax is : "+Tax);
+        }
+   }
+   else
+   {
+     System.out.println("wrong choice");
+   }
+ }
+}
+
+
+
+
+      
