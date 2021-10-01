f544f696687d75524f55d06a0d4c335d63f10f6e
Satyam-79 <satyamverma79@gmail.com>
2021-10-01 22:44:20 +0530

    '

---
+++ learning/Calculator.java
@@ -0,0 +1,80 @@
+class intAddition
+{
+    private int a;
+    private int b;
+    private int c;
+    public intAddition(int x,int y,int z)
+    {
+        a=x;
+        b=y;
+        c=z;
+    }
+    public intAddition(int x,int y)
+    {
+        a=x;
+        b=y;
+        c=0;
+    }
+    void add()
+    {
+        System.out.println("value a:"+a); 
+        System.out.println("value b:"+b);
+        if(c!=0)
+        {
+        System.out.println("value c:"+c); 
+        }
+        int d=a+b+c;
+        System.out.println("Addition:"+d);
+        System.out.println(" "); 
+
+
+    }
+}
+class floatAddition
+{
+    private float a;
+    private float b;
+    private float c;
+    public floatAddition(float x,float y,float z)
+    {
+        a=x;
+        b=y;
+        c=z;
+    }
+    public floatAddition(float x,float y)
+    {
+        a=x;
+        b=y;
+        c=0.0f;
+    }
+    void add()
+    {
+        System.out.println("value a:"+a); 
+        System.out.println("value b:"+b);
+        if(c!=0.0)
+        {
+        System.out.println("value c:"+c); 
+        }
+        float d=a+b+c;
+        System.out.println("Addition:"+d);
+        System.out.println(" "); 
+
+
+    }
+}
+
+class Calculator
+{
+    public static void main(String[] args) 
+    {
+
+        intAddition obj1 = new intAddition(3,4);
+        intAddition obj2 = new intAddition(5,6,7);
+        obj1.add();
+        obj2.add();
+        floatAddition obj3 = new floatAddition(5.4f,6.3f);
+        floatAddition obj4 = new floatAddition(5.4f,6.8f,7.2f);
+        obj3.add();
+        obj4.add();
+    }
+}
--- learning/Tax.java
+++ learning/Tax.java
@@ -1,66 +1,42 @@
 class Tax
-{
+{ 
   public static void main(String args[])
   {
-    int Income = Integer.parseInt(args[0]);
-    string s=args[1];
-char c=args.charAt(1);
-    char Category = ;
-    int Tax;
-    if(Category = )
+    double Income = Double.parseDouble(args[0]);
+    String Category = args[1];
+    String myStr1 = "General";
+    String myStr2 = "Women";
+    
+    double Tax;
+    if(Category.equalsIgnoreCase(myStr1)&&Income<=180000||Category.equalsIgnoreCase(myStr2)&&Income<=190000)
     {
-        if(Income<=180000)
-        {
-            Tax = 0;
-            System.out.println("Tax is : "+Tax);
-        }
-        else if(Income<=500000)
-        {
-             Tax = 180000*0 + (Income - 180000) *0.10;
-             System.out.println("Tax is : "+Tax);
-        }
-        else if(Income<=800000)
-        {
-             Tax = 180000*0+500000*0.10+ (Income - 500000) *0.20;
-             System.out.println("Tax is : "+Tax);
-        }
-        else
-        {
-             Tax = 180000*0+500000*0.10+800000*0.20 (Income - 800000) *0.30;
-             System.out.println("Tax is : "+Tax);
-        }
+        Tax = 0;
+           System.out.println("Tax is : "+Tax);
     }
-    else if(Category = Women)
-    {
-        if(Income<=190000)
+    else if(Income<=180000)
         {
             Tax = 0;
             System.out.println("Tax is : "+Tax);
         }
         else if(Income<=500000)
         {
-             Tax = 180000*0+ (Income - 180000) *0.10;
+             Tax = 180000*0+(Income - 180000)*0.10;
              System.out.println("Tax is : "+Tax);
         }
         else if(Income<=800000)
         {
-             Tax = 180000*0+500000*0.10+ (Income - 500000) *0.20;
+             Tax = 180000*0+500000*0.10+(Income - 500000)*0.20;
              System.out.println("Tax is : "+Tax);
         }
-        else
+        else if(Income>800000)
         {
-             Tax = 180000*0+500000*0.10+800000*0.20 (Income - 800000) *0.30;
+             Tax = 180000*0+500000*0.10+800000*0.20+(Income - 800000)*0.30;
              System.out.println("Tax is : "+Tax);
-        }
-   }
+      }
+   
    else
    {
      System.out.println("wrong choice");
    }
  }
 }
-
-
-
-
-      
