eb75e5d9d3ff34d6e8853c72c1d6d07bd1b79178
Satyam-79 <satyamverma79@gmail.com>
2021-10-02 11:38:56 +0530

    '

---
+++ learning/Calc.java
@@ -0,0 +1,34 @@
+class Calculatorr
+{
+public int Add(int x,int y,int z)
+    {
+        return x+y+z;
+    }
+public int Add(int x,int y)
+    {
+        return x+y;
+    }
+public float Add(float x,float y,float z)
+    {
+        return x+y+z;
+    }
+public float Add(float x,float y)
+    {
+        return x+y;
+    }
+}
+
+
+class Calc
+{
+    public static void main(String[] args) 
+    {
+
+        Calculatorr obj1 = new Calculatorr();
+        System.out.println(obj1.Add(5,6));
+        System.out.println(obj1.Add(1,3,9));
+        System.out.println(obj1.Add(5.3f,6.9f));
+        System.out.println(obj1.Add(5.3f,6.9f,9.4f));
+
+    }
+}
