d8143df9d2961f648b4c36a85c4c8214c7aaf790
Satyam-79 <satyamverma79@gmail.com>
2021-10-31 02:23:48 +0530

    '

--- learning/Factorial.java
+++ learning/Factorial.java
@@ -1,14 +1,11 @@
-class Factorial
-{
-   public static void main(String args[])
-   {
+class Factorial {
+   public static void main(String args[]) {
       int n = Integer.parseInt(args[0]);
       int p = 1;
-      while(n>=1)
-      {
-         p = p*n;
+      while (n >= 1) {
+         p = p * n;
          n--;
       }
-      System.out.println("Factorial is : "+p);
-    }
+      System.out.println("Factorial is : " + p);
+   }
 }
